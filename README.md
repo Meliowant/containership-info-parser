4e33679e4c1856b5ba2e50629b2918e8295df968
Andrii Borovyi <wanderlust@ukr.net>
2017-04-26 00:31:30 +0300

    Added first version of script and its dependencies

---
+++ README.md
@@ -0,0 +1,20 @@
+# Web Scrawler for retrieving data from container-ship.com
+======
+One of my friends asked me if there is any way to retrieve data from [http://www.containership-info.com].
+Firstly I thought about Selenium, but taking into account it is not a quite complex site, and I had no idea about Python3 and Scrapy I decided to test them out.
+This repository contains my tiny script to process and retrieve data from [http://www.containership-info.com] so it can be added to any other editor for processing.
+## Requirements
+* Python3
+* Scrapy
+## Installation
+I suggest to create a virtual environment and run script from it.
+During installation scrapy will need libssl-dev package (at least it is called so on my Debian system), so just install it in advance.
+Simple commands you may use to run everything as fast as possible:
+`sudo apt-get install libssl-dev`
+`virtualenv --python3 venv`
+`. venv/bin/activate`
+`pip install -f < requirements.txt`
+## Usage
+This script supports default scrappy's behaviour, so:
+* To run script as it is `scrapy runspider containership_parser.py`
+* To store data as CSV: `scrapy runspider containership_parser.py -o container.csv -t csv`
---
+++ containership_parser.py
@@ -0,0 +1,51 @@
+import scrapy
+
+
+class SiteSpider(scrapy.Spider):
+    name = 'siteSpider'
+    start_urls = ['http://www.containership-info.com/page_names_a.html',
+                  'http://www.containership-info.com/page_names_b.html',
+                  'http://www.containership-info.com/page_names_c.html',
+                  'http://www.containership-info.com/page_names_d.html',
+                  'http://www.containership-info.com/page_names_e.html',
+                  'http://www.containership-info.com/page_names_f.html',
+                  'http://www.containership-info.com/page_names_g.html',
+                  'http://www.containership-info.com/page_names_h.html',
+                  'http://www.containership-info.com/page_names_i.html',
+                  'http://www.containership-info.com/page_names_j.html',
+                  'http://www.containership-info.com/page_names_k.html',
+                  'http://www.containership-info.com/page_names_l.html',
+                  'http://www.containership-info.com/page_names_m.html',
+                  'http://www.containership-info.com/page_names_n.html',
+                  'http://www.containership-info.com/page_names_o.html',
+                  'http://www.containership-info.com/page_names_p.html',
+                  'http://www.containership-info.com/page_names_q.html',
+                  'http://www.containership-info.com/page_names_r.html',
+                  'http://www.containership-info.com/page_names_s.html',
+                  'http://www.containership-info.com/page_names_t.html',
+                  'http://www.containership-info.com/page_names_u.html',
+                  'http://www.containership-info.com/page_names_v.html',
+                  'http://www.containership-info.com/page_names_w.html',
+                  'http://www.containership-info.com/page_names_x.html',
+                  'http://www.containership-info.com/page_names_y.html',
+                  'http://www.containership-info.com/page_names_z.html',
+                  ]
+
+    def parse(self, response):
+        self.log("Type is {}".format(type(response)))
+        vals = response.xpath('//td/small/small/text()').extract()
+        if vals:
+            ship_no = vals[0][vals[0].find(":")+1:].strip()
+        for ship_info in vals:
+            if len(ship_info[:ship_info.find(":")].strip()) > 0:
+                yield {
+                    'ship number': ship_no,
+                    'ship_field': ship_info[:ship_info.find(":")].strip(),
+                    'ship_field_value': ship_info[ship_info.find(":")+1:].
+                    strip()
+                }
+
+        next_page = response.css("td > a::attr(href)").extract()
+        for p in next_page:
+            if p:
+                yield scrapy.Request(response.urljoin(p), callback=self.parse)
